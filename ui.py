ea9104b52c708ea6078b8b8754d5c437cf246f44
vaceslavlomovcev <balderfreir@yandex.ru>
2022-04-21 16:41:49 +0400

    add magic

--- ui.py
+++ ui.py
@@ -20,6 +20,13 @@ class UI:
             weapon = pygame.image.load(path).convert_alpha()
             self.weapon_graphics.append(weapon)
 
+        # convert magic dictionary
+        self.magic_graphics = []
+        for magic_item in magic_data.values():
+            path = magic_item['graphic']
+            magic = pygame.image.load(path).convert_alpha()
+            self.magic_graphics.append(magic)
+
     def show_bar(self, current, max_amount, bg_rect, color):
         # draw bg
         pygame.draw.rect(self.display_surface, UI_BG_COLOR, bg_rect)
@@ -50,7 +57,7 @@ class UI:
         pygame.draw.rect(self.display_surface, UI_BG_COLOR, bg_rect)  # draw it
 
         if has_switched:
-            #
+            # выделяем бордер другим цветом во время смены оружия
             pygame.draw.rect(self.display_surface, UI_BORDER_COLOR_ACTIVE, bg_rect, 3)
         else:
             pygame.draw.rect(self.display_surface, UI_BORDER_COLOR, bg_rect, 3)
@@ -62,6 +69,12 @@ class UI:
         weapon_rect = weapon_surf.get_rect(center=bg_rect.center)
         self.display_surface.blit(weapon_surf, weapon_rect)
 
+    def magic_overlay(self, magic_index, has_switched):
+        bg_rect = self.selection_box(100, 635, has_switched)  # for magic
+        magic_surf = self.magic_graphics[magic_index]
+        magic_rect = magic_surf.get_rect(center=bg_rect.center)
+        self.display_surface.blit(magic_surf, magic_rect)
+
     def display(self, player):
         # pygame.draw.rect(self.display_surface,'black',self.health_bar_rect)
         self.show_bar(player.health, player.stats['health'], self.health_bar_rect, HEALTH_COLOR)
@@ -69,4 +82,5 @@ class UI:
 
         self.show_exp(player.exp)
         self.weapon_overlay(player.weapon_index, not player.can_switch_weapon)
-        self.selection_box(100, 635, not player.can_switch_weapon)  # for magic
+        self.magic_overlay(player.magic_index, not player.can_switch_weapon)
+
